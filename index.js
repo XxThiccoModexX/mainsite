8f0d683c60c8c2fd4f50e678f46d782c7c555f24
XxThiccoModexX <xxxkidxxxdude@gmail.com>
2023-11-29 14:21:16 +1000

    restructured the enitre thing lol

---
+++ index.js
@@ -0,0 +1,7 @@
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
