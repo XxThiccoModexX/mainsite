f34477d70073fec8d59cc58af62e949efe7ae622
XxThiccoModexX <xxxkidxxxdude@gmail.com>
2023-11-30 12:27:09 +1000

    updated looks

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-  }
+let roblox = document.getElementById('roblox')
+let youtube = document.getElementById('youtube')
+let reddit = document.getElementById('reddit')
+let fumo = document.getElementById('fumo')
+let misty = document.getElementById('misty')
+let onepiece = document.getElementById('onepiece')
+
+var snd = new Audio("audio/oneshot.mp3");
+//roblox.onmouseover = function() {roblox.style.transform = "rotate(20deg)"};
+//roblox.onmouseout = function() {mouseLeave(roblox,"roblox","https://www.roblox.com/home","this is broken atm")};
+//youtube.onmouseover = function() {mouseEnter(youtube,"youtube","https://www.youtube.com","all fun and games until you get to a minecraft smp then its sad")};
+//youtube.onmouseout = function() {mouseLeave2()};
+//reddit.onmouseover = function() {mouseEnter(reddit,"reddit","https://www.reddit.com","RISE UP REDDIT NATION")};
+//reddit.onmouseout = function() {mouseLeave3()};
+//fumo.onmouseover = function() {mouseEnter(fumo,"fumo","https://www.amiami.com","my fumo is my friend it helps me relax")};
+//reddit.onmouseout = function() {mouseLeave3()};
+//misty.onmouseover = function() {mouseEnter(misty,"misty","steam://rungameid/1584090","WHAT WAS SHE COOKING?!?!")};
+//reddit.onmouseout = function() {mouseLeave3()};
+//onepiece.onmouseover = function() {mouseEnter(onepiece,"onepiece","onepiece.html","insane what knowledge i have huh")};
+//reddit.onmouseout = function() {mouseLeave3()};
+//function mouseEnter(idwithnoquotes,id,address,joke) {idwithnoquotes.innerHTML ="<a href="+ address + ">" + joke + "</a>";}
+//function mouseLeave(idwithnoquotes,id,address,joke) {idwithnoquotes.innerHTML ="<a href='https://www.roblox.com/home>'" + joke + "</a>";}
