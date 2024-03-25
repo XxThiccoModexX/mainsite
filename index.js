0b12493e387760b3cf8f603112b9e7371167ec4e
XxThiccoModexX <xxxkidxxxdude@gmail.com>
2024-03-25 19:41:01 +1000

    revamped like the entire website
    lmao

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-let roblox = document.getElementById('roblox')
-let youtube = document.getElementById('youtube')
-let reddit = document.getElementById('reddit')
-let fumo = document.getElementById('fumo')
-let misty = document.getElementById('misty')
-let onepiece = document.getElementById('onepiece')
-let cobalt = document.getElementById('cobalt')
-let mainlink = document.getElementById('mainlink')
-//var snd = new Audio("audio/oneshot.mp3");
-var sndclk = new Audio("audio/click.mp3");
+let dalink = document.getElementById('selecticon')
 
-roblox.onclick = function() {sndclk.play()};
-youtube.onclick = function() {sndclk.play()};
-
-//roblox.onmouseover = function() {roblox.style.transform = "rotate(20deg)"};
-//roblox.onmouseout = function() {mouseLeave(roblox,"roblox","https://www.roblox.com/home","this is broken atm")};
+var cli = new Audio("audio/ok.wav");
+var images = ["backgrounds/tenhours.png", "backgrounds/ambush.png", "backgrounds/engineer.png"]; 
+let bgrng = Math.floor(Math.random() * 2);
+function selectbackground() {
+	document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+}
+dalink.onclick = function() {cli.play();}
 
 
-//youtube.onmouseover = function() {mouseEnter(youtube,"youtube","https://www.youtube.com","all fun and games until you get to a minecraft smp then its sad")};
-//youtube.onmouseout = function() {mouseLeave2()};
-//reddit.onmouseover = function() {mouseEnter(reddit,"reddit","https://www.reddit.com","RISE UP REDDIT NATION")};
-//reddit.onmouseout = function() {mouseLeave3()};
-//fumo.onmouseover = function() {mouseEnter(fumo,"fumo","https://www.amiami.com","my fumo is my friend it helps me relax")};
-//reddit.onmouseout = function() {mouseLeave3()};
-//misty.onmouseover = function() {mouseEnter(misty,"misty","steam://rungameid/1584090","WHAT WAS SHE COOKING?!?!")};
-//reddit.onmouseout = function() {mouseLeave3()};
-//onepiece.onmouseover = function() {mouseEnter(onepiece,"onepiece","onepiece.html","insane what knowledge i have huh")};
-//reddit.onmouseout = function() {mouseLeave3()};
-//function mouseEnter(idwithnoquotes,id,address,joke) {idwithnoquotes.innerHTML ="<a href="+ address + ">" + joke + "</a>";}
-//function mouseLeave(idwithnoquotes,id,address,joke) {idwithnoquotes.innerHTML ="<a href='https://www.roblox.com/home>'" + joke + "</a>";}
+
+
+
+
+
+
+
+//let youtube = document.getElementById('youtube')
+//let reddit = document.getElementById('reddit')
+//let fumo = document.getElementById('fumo')
+//let misty = document.getElementById('misty')
+//let onepiece = document.getElementById('onepiece')
+//let cobalt = document.getElementById('cobalt')
+//let mainlink = document.getElementById('mainlink')
